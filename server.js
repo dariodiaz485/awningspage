a6be1019e8f3ebc5ae0965dd763dd535eea9c646
Dario Diaz <dariopista@icloud.com>
2021-03-04 19:39:15 -0500

    fix error

--- server.js
+++ server.js
@@ -1,13 +1,46 @@
-const express = require("express");
-const SMTPPool = require("nodemailer/lib/smtp-pool");
-const app = express();
+const app = require("./js/app");
+
+const nodemailer = require("nodemailer");
 
 const PORT = process.env.PORT || 5000;
 
+//middleware
+app.use(express.static("AWNINGSPAGE"));
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("hello!!");
+  res.sendFile(__dirname + "/index.html");
 });
 
-SMTPPool.listenerCount(PORT, () => {
+app.post("/", (req, res) => {
+  console.log(req.body);
+
+  const transporter = nodemailer.createTransport({
+    service: "icloud",
+    auth: {
+      user: "dario8876@icloud.come",
+      pass: "Diaz8876",
+    },
+  });
+
+  const mailOptions = {
+    from: req.body.email,
+    to: "dario8876@icloud.come",
+    subject: `Message from ${req.body.email}: ${req.body.subject}`,
+    text: req.body.message,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send("error");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.send("succes");
+    }
+  });
+});
+
+app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
